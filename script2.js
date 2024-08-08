33f320181b4499098ed91afc58624bd2812eee88
taniaBob <akibonorulz@gmail.com>
2024-08-08 12:40:53 -0400

    clase y ejercicio

---
+++ script2.js
@@ -0,0 +1,13 @@
+// ES6
+// var let const
+console.log(cadena);
+var cadena = "hola"
+//  esto es ig  ual que decir
+// var cadena
+// console.log(cadena);
+// cadena = "hola"
+class Consultorio{
+    constructor(nombre, paciente) {
+        
+    }
+} 
